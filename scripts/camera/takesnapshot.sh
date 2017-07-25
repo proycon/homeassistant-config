252555a9979410b07f9f6195c0987cce20cfa1ee
Maarten van Gompel <proycon@anaproy.nl>
2017-07-25 14:54:23 +0200

    wrote a takesnapshot script

---
+++ scripts/camera/takesnapshot.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+source /home/homeautomation/homeassistant/secrets.sh
+cd /home/homeautomation/homeassistant/snapshots/
+
+DATE=`date +%Y-%m-%d_%H:%M:%S`
+
+MODE=$1
+if [ -z "$MODE" ]; then
+    echo "No mode set!" >&2
+    exit 2
+fi
+
+if [ -f "$MODE.lock" ]; then
+    exit 1
+else
+    touch "$MODE.lock"
+fi
+
+if [ "$MODE" == "frontdoor" ]; then
+    fswebcam -S 3 -r 640x480 -d /dev/video1 --save $DATE.$MODE.jpg
+elif [ "$MODE" == "hallupstairs" ]; then
+    fswebcam -S 3 -r 640x480 -d /dev/video0 --save $DATE.$MODE.jpg
+elif [ "$MODE" == "livingroom" ]; then
+    curl -sS --netrc-file $LIVINGROOMCAM_NETRC http://$LIVINGROOMCAM_IP/image/jpeg.cgi -o $DATE.$MODE.jpg
+elif [ "$MODE" == "street" ]; then
+    curl -sS http://$STREETCAM_IP/snapshot.cgi?user=$STREETCAM_USER&pwd=$STREETCAM_PASSWORD -o $DATE.$MODE.jpg
+elif [ "$MODE" == "garden" ]; then
+    curl -sS http://$GARDENCAM_IP/snapshot.cgi?user=$GARDENCAM_USER&pwd=$GARDENCAM_PASSWORD -o $DATE.$MODE.jpg
+else
+    rm "$MODE.lock"
+    echo "No such mode: $MODE" >&2
+    exit 2
+fi
+
+rm "$MODE.lock"
+
+if [ -f $DATE.$MODE.jpg ]; then
+    ln -sf $DATE.$MODE.jpg _$MODE.jpg
+else
+    echo "Failed to take a snapshot for $MODE" >&2
+    exit 3
+fi
+
+exit 0
