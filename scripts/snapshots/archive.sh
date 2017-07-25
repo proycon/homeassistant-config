ad5e741a1a99857d88200fec3a05281c4bd72f8c
Maarten van Gompel <proycon@anaproy.nl>
2017-07-25 13:05:13 +0200

    added camera snapshots and archiving

---
+++ scripts/snapshots/archive.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd snapshots || exit 2
+
+if [ -d archive.3 ]; then
+    rm -rf archive.3
+fi
+if [ -d archive.2 ]; then
+    mv archive.2 archive.3
+fi
+if [ -d archive ]; then
+    mv archive archive.2
+fi
+
+find . -type f -name "2*.jpg" -maxdepth 1 | xargs -I '{}' -n 1  mv '{}' archive/
+
+
