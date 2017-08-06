27217b0993f541cb5c2476bb74e1203941af536b
Maarten van Gompel <proycon@anaproy.nl>
2017-08-06 23:50:33 +0200

    eventsnapshot fix

--- scripts/camera/eventsnapshots.sh
+++ scripts/camera/eventsnapshots.sh
@@ -13,6 +13,6 @@ EVENTDATE=`date +%Y-%m-%d_%H:%M:%S`
 if [ -f event.$CAM.latest.gif ]; then
     mv event.$CAM.latest.gif event.$CAM.previous.gif
 fi
-convert -loop 0 -delay 500 $(find . -name "$DATE*$CAM*jpg" | tail -n 6) $EVENTDATE.$CAM.gif
+convert -loop 0 -delay 500 $(find . -name "$DATE*$CAM*jpg" | sort | tail -n 6) $EVENTDATE.$CAM.gif
 ln -sf $EVENTDATE.$CAM.gif event.$CAM.latest.gif
 rm eventsnapshots.$CAM.lock
