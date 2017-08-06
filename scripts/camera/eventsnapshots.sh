1e13a39fcb03c11dff8fd78e3426176ce222157d
Maarten van Gompel <proycon@anaproy.nl>
2017-08-06 22:46:28 +0200

    fix

--- scripts/camera/eventsnapshots.sh
+++ scripts/camera/eventsnapshots.sh
@@ -4,10 +4,10 @@ CAM=$1
 if [ ! -z "$2" ]; then
     sleep $2
 fi
-if [ -f /tmp/eventsnapshots.$CAM.lock ]; then
+if [ -f eventsnapshots.$CAM.lock ]; then
     exit 0
 fi
-touch /tmp/eventsnapshots.$CAM.lock
+touch eventsnapshots.$CAM.lock
 DATE=`date +%Y-%m-%d_`
 EVENTDATE=`date +%Y-%m-%d_%H:%M:%S`
 if [ -f event.$CAM.latest.gif ]; then
@@ -15,4 +15,4 @@ if [ -f event.$CAM.latest.gif ]; then
 fi
 convert -loop 0 -delay 500 $(find . -name "$DATE*$CAM*jpg" | tail -z -n 6) $EVENTDATE.$CAM.gif
 ln -sf $EVENTDATE.$CAM.gif event.$CAM.latest.gif
-rm /tmp/eventsnapshots.$CAM.lock
+rm eventsnapshots.$CAM.lock
