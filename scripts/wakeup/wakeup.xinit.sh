a47dbdd0e1d84edaba38d43d76d42f313a7c90dd
Maarten van Gompel <proycon@anaproy.nl>
2017-08-19 12:52:16 +0200

    fixes

--- scripts/wakeup/wakeup.xinit.sh
+++ scripts/wakeup/wakeup.xinit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xinit ~/homeassistant/scripts/wakeup/wakeup.xinitrc -- /usr/bin/X -allowMouseOpenFail -no-cursor
+xinit /home/homeautomation/homeassistant/scripts/wakeup/wakeup.xinitrc -- /usr/bin/X -allowMouseOpenFail -nocursor
