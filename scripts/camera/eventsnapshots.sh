59bbc113459f2d9d6634d97ca57e133736289f57
Maarten van Gompel <proycon@anaproy.nl>
2017-09-03 00:29:20 +0200

    handle multiple cams in one script

--- scripts/camera/eventsnapshots.sh
+++ scripts/camera/eventsnapshots.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 cd /home/homeautomation/homeassistant/snapshots/
-CAM=$1
 while [ -f eventsnapshots.lock ]; do
     sleep 1
 done
 touch eventsnapshots.lock
-DATE=`date +%Y-%m-%d_`
-EVENTDATE=`date +%Y-%m-%d_%H:%M:%S`
-#convert -loop 0 -delay 500 $(find . -name "$DATE*$CAM*jpg" | sort | tail -n 6) $EVENTDATE.$CAM.gif
-mv -f event.5.jpg event.6.jpg
-mv -f event.4.jpg event.5.jpg
-mv -f event.3.jpg event.4.jpg
-mv -f event.2.jpg event.3.jpg
-mv -f event.1.jpg event.2.jpg
-IMAGES=$(find . -maxdepth 1 -name "$DATE*$CAM*jpg" | sort | tail -n 6)
-IMAGEARRAY=($IMAGES)
-FIRSTIMAGE=${IMAGEARRAY[0]}
-convert $IMAGES -append -undercolor black -fill yellow -pointsize 16 -gravity north -annotate +0+0 "$FIRSTIMAGE - $EVENTDATE" event.1.jpg
+if [ $# -gt 1 ]; then
+    CAMS=$@
+    NUMCAMS=$#
+else
+    CAMS=($1)
+    NUMCAMS=${#CAMS[@]}
+fi
+mv -f event.5.jpg event.$((NUMCAMS+5)).jpg
+mv -f event.4.jpg event.$((NUMCAMS+4)).jpg
+mv -f event.3.jpg event.$((NUMCAMS+3)).jpg
+mv -f event.2.jpg event.$((NUMCAMS+2)).jpg
+mv -f event.1.jpg event.$((NUMCAMS+1)).jpg
+NUM=0
+for CAM in $@; do
+    NUM=$((NUM+1))
+    DATE=`date +%Y-%m-%d_`
+    EVENTDATE=`date +%Y-%m-%d_%H:%M:%S`
+    #convert -loop 0 -delay 500 $(find . -name "$DATE*$CAM*jpg" | sort | tail -n 6) $EVENTDATE.$CAM.gif
+    IMAGES=$(find . -maxdepth 1 -name "$DATE*$CAM*jpg" | sort | tail -n 6)
+    IMAGEARRAY=($IMAGES)
+    FIRSTIMAGE=${IMAGEARRAY[0]}
+    convert $IMAGES -append -undercolor black -fill yellow -pointsize 16 -gravity north -annotate +0+0 "$FIRSTIMAGE - $EVENTDATE" event.$NUM.jpg
+done
 rm eventsnapshots.lock
