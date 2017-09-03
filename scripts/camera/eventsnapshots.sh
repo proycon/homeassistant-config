3baffddfa954c965ae3322b25139f1a31657432f
Maarten van Gompel <proycon@anaproy.nl>
2017-09-03 12:08:20 +0200

    event snapshot fixes

--- scripts/camera/eventsnapshots.sh
+++ scripts/camera/eventsnapshots.sh
@@ -17,14 +17,14 @@ mv -f event.3.jpg event.$((NUMCAMS+3)).jpg
 mv -f event.2.jpg event.$((NUMCAMS+2)).jpg
 mv -f event.1.jpg event.$((NUMCAMS+1)).jpg
 NUM=0
-for CAM in $@; do
+DATE=`date +%Y-%m-%d_`
+EVENTDATE=`date +%Y-%m-%d_%H:%M:%S`
+for CAM in ${CAMS[@]}; do
     NUM=$((NUM+1))
-    DATE=`date +%Y-%m-%d_`
-    EVENTDATE=`date +%Y-%m-%d_%H:%M:%S`
     #convert -loop 0 -delay 500 $(find . -name "$DATE*$CAM*jpg" | sort | tail -n 6) $EVENTDATE.$CAM.gif
     IMAGES=$(find . -maxdepth 1 -name "$DATE*$CAM*jpg" | sort | tail -n 6)
     IMAGEARRAY=($IMAGES)
     FIRSTIMAGE=${IMAGEARRAY[0]}
-    convert $IMAGES -append -undercolor black -fill yellow -pointsize 16 -gravity north -annotate +0+0 "$FIRSTIMAGE - $EVENTDATE" event.$NUM.jpg
+    convert $IMAGES -append -undercolor black -fill yellow -pointsize 16 -gravity north -annotate +0+0 "$FIRSTIMAGE - $EVENTDATE" event.$NUM.jpg &
 done
 rm eventsnapshots.lock
