27fc75953e7052478029f6ae87997c2bf69e0ba2
Maarten van Gompel <proycon@anaproy.nl>
2018-01-01 13:47:26 +0100

    record

--- scripts/voice/hotworddetector.py
+++ scripts/voice/hotworddetector.py
@@ -1,6 +1,7 @@
 import snowboydecoder
 import sys
 import signal
+import os
 
 interrupted = False
 
@@ -14,6 +15,11 @@ def interrupt_callback():
     global interrupted
     return interrupted
 
+def onhotword():
+    snowboydecoder.play_audio_file("/home/homeautomation/homeassistant/media/computerbeep_5.wav")
+    os.system("rec /tmp/recording.wav rate 16k trim 0 5 silence -l 1 0.1 3% 1 1.2 3%")
+    snowboydecoder.play_audio_file("/home/homeautomation/homeassistant/media/computerbeep_65.wav")
+
 if len(sys.argv) == 1:
     print("Error: need to specify model name")
     print("Usage: python demo.py your.model")
@@ -24,11 +30,11 @@ model = sys.argv[1]
 # capture SIGINT signal, e.g., Ctrl+C
 signal.signal(signal.SIGINT, signal_handler)
 
-detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
+detector = snowboydecoder.HotwordDetector(model, sensitivity=0.45)
 print('Listening... Press Ctrl+C to exit')
 
 # main loop
-detector.start(detected_callback=lambda: snowboydecoder.play_audio_file("/home/homeautomation/homeassistant/media/computerbeep_5.wav"),  #snowboydecoder.play_audio_file,
+detector.start(detected_callback=onhotword,  #snowboydecoder.play_audio_file,
                interrupt_check=interrupt_callback,
                sleep_time=0.03)
 
