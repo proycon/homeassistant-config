43a0efde9696aba04fc88f1124f09d9998d74166
Maarten van Gompel <proycon@anaproy.nl>
2020-01-18 16:10:35 +0100

    lirc setup

--- setup-raspberry-pi.sh
+++ setup-raspberry-pi.sh
@@ -19,16 +19,27 @@ systemctl enable ssh || exit 2
 systemctl set-default multi-user.target || exit 2 #no graphical UI by default
 ln -fs /etc/systemd/system/autologin@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
 
-apt install aptitude tmux git gcc make lirc lirc-compat-remotes zsh kodi kodi-audioencoder-flac kodi-audioencoder-lame kodi-audioencoder-vorbis wiringpi python3-virtualenv || exit 1
+apt install aptitude tmux git gcc make zsh kodi kodi-audioencoder-flac kodi-audioencoder-lame kodi-audioencoder-vorbis wiringpi python3-virtualenv || exit 1
 
+if grep "MYSETUP: LIRC" /boot/config.txt; then
+    echo "lirc already set up"
+else
+    echo "#MYSETUP: LIRC" >> /boot/config.txt
+    if [ $PI -eq 1 ]; then
+        echo "dtoverlay=gpio-ir-tx,gpio_out_pin=17" >> /boot/config.txt
+    elif [ $PI -eq 2 ]; then
+        echo "dtoverlay=gpio-ir,gpio_pin=24" >> /boot/config.txt
+        echo "dtoverlay=gpio-ir-tx,gpio_pin=4" >> /boot/config.txt
+    fi
+
+    echo "REBOOT FIRST NOW AND THEN RUN THIS SCRIPT AGAIN"
+    exit 0
+fi
+
+apt install lirc lirc-compat-remotes || exit 1
 
 if [ ! -d /home/homeautomation ]; then
     useradd -s /bin/zsh -m -d /home/homeautomation -G pi,adm,dialout,cdrom,sudo,audio,video,plugdev,users,input,netdev,spi,i2c,gpio homeautomation || exit 1
-    if [ $PI -eq 1 ]; then
-        echo "dtoverlay=lirc-rpi,gpio_out_pin=17" >> /boot/config.txt
-    elif [ $PI -eq 2 ]; then
-        echo "dtoverlay=lirc-rpi,gpio_out_pin=4,gpio_in_pin=24" >> /boot/config.txt
-    fi
 fi
 
 if [ ! -d /home/homeautomation/homeassistant ]; then
