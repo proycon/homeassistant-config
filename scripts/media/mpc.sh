74ce33066d4d2cf684c33f5a95b9a1a8c254be42
Maarten van Gompel <proycon@anaproy.nl>
2020-01-19 18:01:40 +0100

    debug

--- scripts/media/mpc.sh
+++ scripts/media/mpc.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 export MPD_HOST="proycon@mediaserver.anaproy.lxd"
-if [ -z "$3" ] || [[ "$3" == "no" ]]; then
-    mpc search "$1" "$2" | mpc add
+TYPE=$1
+VALUE=$2
+FILTER=$3
+echo "calling mpc search and play:">&2
+echo "type: $TYPE">&2
+echo "value: $VALUE">&2
+echo "filter: $FILTER">&2
+if [ -z "$FILTER" ] || [[ "$FILTER" == "no" ]]; then
+    mpc search "$TYPE" "$VALUE" | mpc add
 elif [ -z "$4" ]; then
-    mpc search "$1" "$2" | grep -e "$3" | mpc add
+    mpc search "$TYPE" "$VALUE" | grep -e "$FILTER" | mpc add
 else
-    mpc search "$1" "$2" | grep -e "$3" -e "$4" | mpc add
+    mpc search "$TYPE" "$VALUE" | grep -e "$FILTER" -e "$4" | mpc add
 fi
 mpc play
