1f7610f9c8c7a6f6712daf56abf0c8b6f54b3196
proycon <proycon@anaproy.nl>
2020-05-15 14:35:15 +0200

    fixed picture-elements

--- scripts/media/mpc.sh
+++ scripts/media/mpc.sh
@@ -3,10 +3,9 @@ export MPD_HOST="proycon@mediaserver.anaproy.lxd"
 TYPE=$1
 VALUE=$2
 FILTER=$3
-echo "calling mpc search and play:">&2
-echo "type: $TYPE">&2
-echo "value: $VALUE">&2
-echo "filter: $FILTER">&2
+echo "calling mpc search and play:"
+echo "type: $TYPE, value: $VALUE, filter: $FILTER"
+echo "type: $TYPE, value: $VALUE, filter: $FILTER" > /tmp/lastmpc
 if [ -z "$FILTER" ] || [[ "$FILTER" == "no" ]]; then
     mpc search "$TYPE" "$VALUE" | mpc add
 elif [ -z "$4" ]; then
