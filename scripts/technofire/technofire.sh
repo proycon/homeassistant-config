ec664de08905afa4b2fe50d9411c17517768a8fa
Maarten van Gompel <proycon@anaproy.nl>
2022-01-23 15:09:50 +0100

    accept third parameter

--- scripts/technofire/technofire.sh
+++ scripts/technofire/technofire.sh
@@ -2,4 +2,5 @@
 pkill -f technofire.py > /dev/null 2>/dev/null
 echo $1
 echo $2
-/home/homeautomation/homeassistant/scripts/technofire/technofire.py "$1" "$2"
+echo $3
+/home/homeautomation/homeassistant/scripts/technofire/technofire.py "$1" "$2" "$3"
