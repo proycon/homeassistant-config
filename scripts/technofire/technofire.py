2f82eeef02fa4ddf6c3e87320b61aa9c506d6404
Maarten van Gompel <proycon@anaproy.nl>
2022-01-23 14:30:49 +0100

    updated pi4 led scrip control

--- scripts/technofire/technofire.py
+++ scripts/technofire/technofire.py
@@ -15,7 +15,10 @@ from neopixel import *
 colorstate = {}
 
 # LED strip configuration:
-LED_COUNT      = 30      # Number of LED pixels.
+try:
+    LED_COUNT = int(sys.argv[3])     # Set to 0 for darkest and 255 for brightest
+except:
+    LED_COUNT  = 30      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
