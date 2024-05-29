ab6a9f7ddc09d63714ac2d1ab566e9d418964cb1
Maarten van Gompel <proycon@anaproy.nl>
2024-05-29 21:44:06 +0200

    give virtualenv access to system packages (=container)

--- setup.sh
+++ setup.sh
@@ -8,11 +8,11 @@ fi
 cd $HADIR
 
 if [ ! -d "$HADIR/env" ]; then
-    python3 -m venv env
+    python3 -m venv --system-site-packages env
 fi
 
 . env/bin/activate
-pip install wheel psycopg2 numpy scipy vext vext.gi
+#pip install wheel psycopg2 numpy scipy vext vext.gi
 
 if [ -n "$HASS_VERSION" ]; then
     echo "Installing explicit HASS version: $HASS_VERSION"
